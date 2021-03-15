50f03ccf859720e2470216227d45328847c92f0d
Ryan Halley <ryan.halley@panoramic.systems>
2021-03-15 17:41:22 +0000

    Fix for handling content without an endline delimiter.

--- PanoramicData.Vtl.Test/VariableTests.cs
+++ PanoramicData.Vtl.Test/VariableTests.cs
@@ -4,23 +4,39 @@ using Xunit;
 
 namespace PanoramicData.Vtl.Test
 {
-	public class VariableTests : BaseTest
-	{
-		[Fact]
-		public void SimpleParseSucceeds()
-		{
-			const string text = "Test File for $purpose\r\nwith description ${description}\r\n";
-			var variables = new Dictionary<string, object>
-			{
-				["purpose"] = "testing",
-				["description"] = "'the description'",
-			};
-			var vtlParser = new VtlParser();
-			var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
+    public class VariableTests : BaseTest
+    {
+        [Fact]
+        public void SimpleParseSucceeds()
+        {
+            const string text = "Test File for $purpose\r\nwith description ${description}\r\n";
+            var variables = new Dictionary<string, object>
+            {
+                ["purpose"] = "testing",
+                ["description"] = "'the description'",
+            };
+            var vtlParser = new VtlParser();
+            var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
 
-			isSuccess.Should().BeTrue();
+            isSuccess.Should().BeTrue();
 
-			parsedText.Should().Be("Test File for testing\r\nwith description 'the description'\r\n");
-		}
-	}
+            parsedText.Should().Be("Test File for testing\r\nwith description 'the description'\r\n");
+        }
+
+        [Fact]
+        public void SimpleParseTwoSucceeds()
+        {
+            const string text = "!\r\nhostname ${hostname}";
+            var variables = new Dictionary<string, object>
+            {
+                ["hostname"] = "TESTHOSTNAME",
+            };
+            var vtlParser = new VtlParser();
+            var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
+
+            isSuccess.Should().BeTrue();
+
+            parsedText.Should().Be("!\r\nhostname TESTHOSTNAME");
+        }
+    }
 }
