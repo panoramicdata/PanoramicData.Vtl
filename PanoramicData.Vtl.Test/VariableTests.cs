b28bedf89e011d2fde3048af4700b6bb19912e39
David Bond <david.bond@panoramicdata.com>
2022-11-21 00:36:39 +0000

    Moved to netstandard2.0 and added spelling checks

--- PanoramicData.Vtl.Test/VariableTests.cs
+++ PanoramicData.Vtl.Test/VariableTests.cs
@@ -4,55 +4,55 @@ using Xunit;
 
 namespace PanoramicData.Vtl.Test
 {
-    public class VariableTests : BaseTest
-    {
-        [Fact]
-        public void SimpleParseSucceeds()
-        {
-            const string text = "Test File for $purpose\r\nwith description ${description}\r\n";
-            var variables = new Dictionary<string, object>
-            {
-                ["purpose"] = "testing",
-                ["description"] = "'the description'",
-            };
-            var vtlParser = new VtlParser();
-            var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
+	public class VariableTests : BaseTest
+	{
+		[Fact]
+		public void SimpleParseSucceeds()
+		{
+			const string text = "Test File for $purpose\r\nwith description ${description}\r\n";
+			var variables = new Dictionary<string, object>
+			{
+				["purpose"] = "testing",
+				["description"] = "'the description'",
+			};
+			var vtlParser = new VtlParser();
+			var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
 
-            isSuccess.Should().BeTrue();
+			isSuccess.Should().BeTrue();
 
-            parsedText.Should().Be("Test File for testing\r\nwith description 'the description'\r\n");
-        }
+			parsedText.Should().Be("Test File for testing\r\nwith description 'the description'\r\n");
+		}
 
-        [Fact]
-        public void SimpleParseTwoSucceeds()
-        {
-            const string text = "!\r\nhostname ${hostname}";
-            var variables = new Dictionary<string, object>
-            {
-                ["hostname"] = "TESTHOSTNAME",
-            };
-            var vtlParser = new VtlParser();
-            var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
+		[Fact]
+		public void SimpleParseTwoSucceeds()
+		{
+			const string text = "!\r\nhostname ${hostname}";
+			var variables = new Dictionary<string, object>
+			{
+				["hostname"] = "TESTHOSTNAME",
+			};
+			var vtlParser = new VtlParser();
+			var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
 
-            isSuccess.Should().BeTrue();
+			isSuccess.Should().BeTrue();
 
-            parsedText.Should().Be("!\r\nhostname TESTHOSTNAME");
-        }
+			parsedText.Should().Be("!\r\nhostname TESTHOSTNAME");
+		}
 
-        [Fact]
-        public void VariablePrefixParseSucceeds()
-        {
-            const string text = "Test File for +purpose";
-            var variables = new Dictionary<string, object>
-            {
-                ["purpose"] = "testing",
-            };
-            var vtlParser = new VtlParser(new VtlParserOptions { VariablePrefixCharacter = '+'});
-            var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
+		[Fact]
+		public void VariablePrefixParseSucceeds()
+		{
+			const string text = "Test File for +purpose";
+			var variables = new Dictionary<string, object>
+			{
+				["purpose"] = "testing",
+			};
+			var vtlParser = new VtlParser(new VtlParserOptions { VariablePrefixCharacter = '+' });
+			var isSuccess = vtlParser.TryParse(text, variables, out var parsedText);
 
-            isSuccess.Should().BeTrue();
+			isSuccess.Should().BeTrue();
 
-            parsedText.Should().Be("Test File for testing");
-        }
-    }
+			parsedText.Should().Be("Test File for testing");
+		}
+	}
 }
--- PanoramicData.Vtl/VtlParser.cs
+++ PanoramicData.Vtl/VtlParser.cs
@@ -6,190 +6,201 @@ using System.Text;
 
 namespace PanoramicData.Vtl
 {
-    public class VtlParser
-    {
-        private readonly VtlParserOptions vtlParserOptions;
-        private readonly Stack<ParseMode> conditionStack = new();
-        private const char VARIABLE_PREFIX_CHARACTER = '$';
+	public class VtlParser
+	{
+		private readonly VtlParserOptions _vtlParserOptions;
+		private readonly Stack<ParseMode> _conditionStack = new();
+		private const char VARIABLE_PREFIX_CHARACTER = '$';
 
-        public VtlParser(VtlParserOptions? vtlParserOptions = null)
-        {
-            this.vtlParserOptions = vtlParserOptions ?? new();
-        }
+		public VtlParser(VtlParserOptions? vtlParserOptions = null)
+		{
+			_vtlParserOptions = vtlParserOptions ?? new();
+		}
 
-        public bool TryParse(FileInfo fileInfo, Dictionary<string, object> variables, out string result)
-            => TryParse(File.ReadAllText(fileInfo.FullName), variables, out result);
+		public bool TryParse(FileInfo fileInfo, Dictionary<string, object> variables, out string result)
+			=> TryParse(File.ReadAllText(fileInfo.FullName), variables, out result);
 
-        public bool TryParse(string text, Dictionary<string, object> variables, out string result)
-        {
-            try
-            {
-                conditionStack.Push(ParseMode.Root);
-                var sb = new StringBuilder();
-                var lineNumber = 0;
-                foreach (var line in GetLines(text))
-                {
-                    lineNumber++;
+		public bool TryParse(string text, Dictionary<string, object> variables, out string result)
+		{
+			try
+			{
+				_conditionStack.Push(ParseMode.Root);
+				var sb = new StringBuilder();
+				var lineNumber = 0;
+				foreach (var line in GetLines(text))
+				{
+					lineNumber++;
 
-                    // Set support
-                    var trimmedLine = line.TrimEnd();
-                    if (trimmedLine.StartsWith("#set (") && trimmedLine.EndsWith(")"))
-                    {
-                        ProcessSet(line.Substring("#set (".Length, trimmedLine.Length - "#set ()".Length), variables);
-                        continue;
-                    }
+					// Set support
+					var trimmedLine = line.TrimEnd();
+					if (trimmedLine.StartsWith("#set (") && trimmedLine.EndsWith(")"))
+					{
+						ProcessSet(line.Substring("#set (".Length, trimmedLine.Length - "#set ()".Length), variables);
+						continue;
+					}
 
-                    // Conditional support (if/elseif/else/end)
-                    if (trimmedLine.StartsWith("#if (") && trimmedLine.EndsWith(")"))
-                    {
-                        var ifExpression = trimmedLine.Substring("#if (".Length, trimmedLine.Length - "#if ()".Length);
-                        conditionStack.Push(Calculate(ifExpression, variables)
-                            ? ParseMode.IfActive
-                            : ParseMode.IfInactive);
-                        continue;
-                    }
-                    if (trimmedLine.StartsWith("#elseif (") && trimmedLine.EndsWith(")"))
-                    {
-                        switch (conditionStack.Pop())
-                        {
-                            case ParseMode.IfActive:
-                            case ParseMode.ElseIfActive:
-                                conditionStack.Push(ParseMode.IfHandled);
-                                break;
-                            case ParseMode.IfInactive:
-                            case ParseMode.ElseIfInactive:
-                                conditionStack.Push(Calculate(trimmedLine.Substring("#elseif (".Length, trimmedLine.Length - "#elseif ()".Length), variables)
-                                    ? ParseMode.ElseIfActive
-                                    : ParseMode.ElseIfInactive);
-                                break;
-                            default:
-                                throw new ParseException($"Unexpected elseif on line {lineNumber}");
-                        }
-                        continue;
-                    }
-                    if (trimmedLine.StartsWith("#else"))
-                    {
-                        var parseMode = conditionStack.Pop();
-                        switch (parseMode)
-                        {
-                            case ParseMode.IfActive:
-                            case ParseMode.ElseIfActive:
-                            case ParseMode.IfHandled:
-                                conditionStack.Push(ParseMode.IfHandled);
-                                break;
-                            case ParseMode.IfInactive:
-                            case ParseMode.ElseIfInactive:
-                                conditionStack.Push(ParseMode.ElseActive);
-                                break;
-                            default:
-                                throw new ParseException($"Unexpected elseif on line {lineNumber}");
-                        }
-                        continue;
-                    }
-                    if (trimmedLine.StartsWith("#end"))
-                    {
-                        if (conditionStack.Count == 0)
-                        {
-                            throw new ParseException($"Unexpected end on line {lineNumber}");
-                        }
-                        conditionStack.Pop();
-                        continue;
-                    }
+					// Conditional support (if/elseif/else/end)
+					if (trimmedLine.StartsWith("#if (") && trimmedLine.EndsWith(")"))
+					{
+						var ifExpression = trimmedLine.Substring("#if (".Length, trimmedLine.Length - "#if ()".Length);
+						_conditionStack.Push(Calculate(ifExpression, variables)
+							? ParseMode.IfActive
+							: ParseMode.IfInactive);
+						continue;
+					}
 
-                    // TODO - loops support (foreach/end)
-                    // TODO - include support
-                    // TODO - parse support
-                    // TODO - evaluate support
-                    // TODO - break support
-                    // TODO - stop support
-                    // TODO - velocimacros support
-                    // TODO - math support
-                    // TODO - range operator support
+					if (trimmedLine.StartsWith("#elseif (") && trimmedLine.EndsWith(")"))
+					{
+						switch (_conditionStack.Pop())
+						{
+							case ParseMode.IfActive:
+							case ParseMode.ElseIfActive:
+								_conditionStack.Push(ParseMode.IfHandled);
+								break;
+							case ParseMode.IfInactive:
+							case ParseMode.ElseIfInactive:
+								_conditionStack.Push(Calculate(trimmedLine.Substring("#elseif (".Length, trimmedLine.Length - "#elseif ()".Length), variables)
+									? ParseMode.ElseIfActive
+									: ParseMode.ElseIfInactive);
+								break;
+							default:
+								throw new ParseException($"Unexpected elseif on line {lineNumber}");
+						}
 
-                    switch (conditionStack.Peek())
-                    {
-                        case ParseMode.Root:
-                        case ParseMode.Normal:
-                        case ParseMode.ForEach:
-                        case ParseMode.IfActive:
-                        case ParseMode.ElseIfActive:
-                        case ParseMode.ElseActive:
-                            sb.Append(Replace(line, variables));
-                            break;
-                    }
-                }
-                result = sb.ToString();
-                return true;
-            }
-            catch (ParseException)
-            {
-                result = string.Empty;
-                return false;
-            }
-        }
+						continue;
+					}
 
-        private bool Calculate(string text, Dictionary<string, object> variables)
-        {
-            var variablePrefixString = vtlParserOptions.VariablePrefixCharacter ?? VARIABLE_PREFIX_CHARACTER;
+					if (trimmedLine.StartsWith("#else"))
+					{
+						var parseMode = _conditionStack.Pop();
+						switch (parseMode)
+						{
+							case ParseMode.IfActive:
+							case ParseMode.ElseIfActive:
+							case ParseMode.IfHandled:
+								_conditionStack.Push(ParseMode.IfHandled);
+								break;
+							case ParseMode.IfInactive:
+							case ParseMode.ElseIfInactive:
+								_conditionStack.Push(ParseMode.ElseActive);
+								break;
+							default:
+								throw new ParseException($"Unexpected elseif on line {lineNumber}");
+						}
 
-            foreach (var kvp in variables)
-            {
-                text = text
-                    .Replace($"{variablePrefixString}{kvp.Key}", kvp.Value.ToString())
-                    .Replace($"{variablePrefixString}{{{kvp.Key}}}", kvp.Value.ToString());
-            }
-            var expression = new ExtendedExpression(text);
-            return expression.Evaluate() as bool?
-                ?? throw new ParseException($"Function does not evaluate as a boolean: '{text}'");
-        }
+						continue;
+					}
 
-        private string Replace(string line, Dictionary<string, object> variables)
-        {
-            var variablePrefixString = vtlParserOptions.VariablePrefixCharacter ?? VARIABLE_PREFIX_CHARACTER;
+					if (trimmedLine.StartsWith("#end"))
+					{
+						if (_conditionStack.Count == 0)
+						{
+							throw new ParseException($"Unexpected end on line {lineNumber}");
+						}
 
-            foreach (var variable in variables)
-            {
-                // Form: $variableName
-                line = line.Replace($"{variablePrefixString}{variable.Key}", variable.Value.ToString());
-                // Form: ${variableName}
-                line = line.Replace($"{variablePrefixString}{{{variable.Key}}}", variable.Value.ToString());
-            }
-            return line;
-        }
+						_conditionStack.Pop();
+						continue;
+					}
 
-        private void ProcessSet(string setSpec, Dictionary<string, object> variables)
-        {
-            var keyValuePair = setSpec.Split('=');
-            if (keyValuePair.Length != 2)
-            {
-                throw new ParseException("");
-            }
-            variables[keyValuePair[0].Trim(' ').TrimStart(VARIABLE_PREFIX_CHARACTER)] = keyValuePair[1].Trim(' ').Trim('"');
-        }
+					// TODO - loops support (foreach/end)
+					// TODO - include support
+					// TODO - parse support
+					// TODO - evaluate support
+					// TODO - break support
+					// TODO - stop support
+					// TODO - velocimacros support
+					// TODO - math support
+					// TODO - range operator support
 
-        public IEnumerable<string> GetLines(string text)
-        {
-            var autoDetectedNewlineString = text.Any(t => t == '\r')
-                ? "\r\n"
-                : "\n";
-            var sb = new StringBuilder();
-            for (var cursor = 0; cursor < text.Length; cursor++)
-            {
-                switch (text[cursor])
-                {
-                    case '\r':
-                        break;
-                    case '\n':
-                        sb.Append(vtlParserOptions.NewLineOverride ?? autoDetectedNewlineString);
-                        yield return sb.ToString();
-                        sb.Clear();
-                        break;
-                    default:
-                        sb.Append(text[cursor]);
-                        break;
-                }
-            }
-            yield return sb.ToString();
-        }
-    }
+					switch (_conditionStack.Peek())
+					{
+						case ParseMode.Root:
+						case ParseMode.Normal:
+						case ParseMode.ForEach:
+						case ParseMode.IfActive:
+						case ParseMode.ElseIfActive:
+						case ParseMode.ElseActive:
+							sb.Append(Replace(line, variables));
+							break;
+					}
+				}
+
+				result = sb.ToString();
+				return true;
+			}
+			catch (ParseException)
+			{
+				result = string.Empty;
+				return false;
+			}
+		}
+
+		private bool Calculate(string text, Dictionary<string, object> variables)
+		{
+			var variablePrefixString = _vtlParserOptions.VariablePrefixCharacter ?? VARIABLE_PREFIX_CHARACTER;
+
+			foreach (var kvp in variables)
+			{
+				text = text
+					.Replace($"{variablePrefixString}{kvp.Key}", kvp.Value.ToString())
+					.Replace($"{variablePrefixString}{{{kvp.Key}}}", kvp.Value.ToString());
+			}
+
+			var expression = new ExtendedExpression(text);
+			return expression.Evaluate() as bool?
+				?? throw new ParseException($"Function does not evaluate as a boolean: '{text}'");
+		}
+
+		private string Replace(string line, Dictionary<string, object> variables)
+		{
+			var variablePrefixString = _vtlParserOptions.VariablePrefixCharacter ?? VARIABLE_PREFIX_CHARACTER;
+
+			foreach (var variable in variables)
+			{
+				// Form: $variableName
+				line = line.Replace($"{variablePrefixString}{variable.Key}", variable.Value.ToString());
+				// Form: ${variableName}
+				line = line.Replace($"{variablePrefixString}{{{variable.Key}}}", variable.Value.ToString());
+			}
+
+			return line;
+		}
+
+		private void ProcessSet(string setSpec, Dictionary<string, object> variables)
+		{
+			var keyValuePair = setSpec.Split('=');
+			if (keyValuePair.Length != 2)
+			{
+				throw new ParseException("");
+			}
+
+			variables[keyValuePair[0].Trim(' ').TrimStart(VARIABLE_PREFIX_CHARACTER)] = keyValuePair[1].Trim(' ').Trim('"');
+		}
+
+		public IEnumerable<string> GetLines(string text)
+		{
+			var autoDetectedNewlineString = text.Any(t => t == '\r')
+				? "\r\n"
+				: "\n";
+			var sb = new StringBuilder();
+			for (var cursor = 0; cursor < text.Length; cursor++)
+			{
+				switch (text[cursor])
+				{
+					case '\r':
+						break;
+					case '\n':
+						sb.Append(_vtlParserOptions.NewLineOverride ?? autoDetectedNewlineString);
+						yield return sb.ToString();
+						sb.Clear();
+						break;
+					default:
+						sb.Append(text[cursor]);
+						break;
+				}
+			}
+
+			yield return sb.ToString();
+		}
+	}
 }
