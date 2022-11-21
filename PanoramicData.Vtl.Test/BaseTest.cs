a2952e02382c9bbbeb895f86c5bbdf0eb1857c62
David Bond <david.bond@panoramicdata.com>
2022-11-21 00:48:18 +0000

    Unit tests fixed

--- PanoramicData.Vtl.Test/BaseTest.cs
+++ PanoramicData.Vtl.Test/BaseTest.cs
@@ -4,7 +4,7 @@ public abstract class BaseTest
 {
 	protected BaseTest(VtlParserOptions? vtlParserOptions = null)
 	{
-		VtlParser = new VtlParser(vtlParserOptions);
+		VtlParser = new VtlParser(vtlParserOptions ?? new());
 	}
 
 	public VtlParser VtlParser { get; }
